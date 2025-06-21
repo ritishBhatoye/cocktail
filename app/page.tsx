a8416afb596ea33fd8fd5099d29a294c49915471
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-21 20:41:00 +0530

    Add HeroSection component to Home layout for enhanced user experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Navbar from "@/components/global/Navbar";
+import HeroSection from "@/components/global/HeroSection";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,6 +11,7 @@ const Home = () => {
   return (
     <main>
       <Navbar />
+      <HeroSection />
     </main>
   );
 };
