8d38ab9ba982a0f88c6cc79fc34b584696bf6a1c
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-21 19:15:28 +0530

    Add GSAP animations to Navbar component and include logo image

--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -1,12 +1,37 @@
+"use client";
+
 import { navLinks } from "@/constants";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
   return (
     <nav>
       <div>
         <Link href={"#home"} className="flex items-center gap-2">
+          <Image src={"/images/logo.png"} alt="logo" width={50} height={50} />
           <p>Velvet Pour</p>
         </Link>
         <ul>
