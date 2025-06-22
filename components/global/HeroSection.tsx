1bad2252967202bd9680803203e257e2d9dc3ecf
Ritish Bhatoye <ritishrahul87@gmail.com>
2025-06-22 09:22:13 +0530

    Add scroll-triggered GSAP animations to HeroSection for enhanced visual dynamics and interactivity.

--- components/global/HeroSection.tsx
+++ components/global/HeroSection.tsx
@@ -27,6 +27,17 @@ const HeroSection = () => {
       stagger: 0.06,
       delay: 1,
     });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: " bottom top",
+          scrub: true,
+        },
+      })
+      .to("right-leaf", { y: 200 }, 0)
+      .to("left-leaf", { y: -200 }, 0);
   }, []);
   return (
     <>
